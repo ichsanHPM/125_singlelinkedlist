5e9e62a03cf276dc8d0aba1c2b29e2f07dc2c18f
ichsanHPM <ichsan.helmi.ft22@mail.umy.ac.id>
2023-05-18 18:27:28 +0700

    while

--- 125_singlelinkedlist/125_singlelinkedlist.cpp
+++ 125_singlelinkedlist/125_singlelinkedlist.cpp
@@ -91,6 +91,16 @@ bool listEmpty() {
 
 void traverse() {
     if (listEmpty()) {
+        cout << "List Kosong" << endl;
+        system("pause");
+        system("cls");
+        return;
+    }
+    else {
+        Node* currentNode = START;
+        while (currentNode != NULL) {
+
+        }
 
     }
 }
