ac9eb488b10fa37e4779e85f4de8e3f36df95f42
ichsanHPM <ichsan.helmi.ft22@mail.umy.ac.id>
2023-05-18 17:53:39 +0700

    if start

--- 125_singlelinkedlist/125_singlelinkedlist.cpp
+++ 125_singlelinkedlist/125_singlelinkedlist.cpp
@@ -10,7 +10,22 @@ struct Node {
 Node* START = NULL;
 
 void addNode() {
+    int nim;
+    string nama;
+    Node* nodeBaru = new Node();
+    cout << "Masukkan NIM: ";
+    cin >> nim;
+    cout << "Masukkan Nama: ";
+    cin >> nama;
+    nodeBaru->noMhs = nim;
+    nodeBaru->name = nama;
 
+    if (START == NULL || nim <= START->noMhs) {
+        if (START != NULL && nim == START->noMhs)
+        {
+
+        }
+    }
 }
 
 int main()
