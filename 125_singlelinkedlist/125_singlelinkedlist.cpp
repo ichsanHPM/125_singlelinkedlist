9b525ceef53e8ca875b00bc8d4e85c0525161e56
ichsanHPM <ichsan.helmi.ft22@mail.umy.ac.id>
2023-05-18 18:06:42 +0700

    bool

--- 125_singlelinkedlist/125_singlelinkedlist.cpp
+++ 125_singlelinkedlist/125_singlelinkedlist.cpp
@@ -35,8 +35,21 @@ void addNode() {
 
     while ((current != NULL) && (nim >= current->noMhs))
     {
-
+        if (nim == current->noMhs)
+        {
+            cout << "NIM sudah ada" << endl;
+            return;
+        }
+        previous = current;
+        current = current->next;
     }
+
+    nodeBaru->next = current;
+    previous->next = nodeBaru;
+}
+
+bool serachNode(int nim, Node* current, Node* previous) {
+
 }
 
 
