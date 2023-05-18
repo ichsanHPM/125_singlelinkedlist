69df15aa36edeeeaa486b5bc8385ce7c11dbf75b
ichsanHPM <ichsan.helmi.ft22@mail.umy.ac.id>
2023-05-18 18:14:18 +0700

    bool

--- 125_singlelinkedlist/125_singlelinkedlist.cpp
+++ 125_singlelinkedlist/125_singlelinkedlist.cpp
@@ -59,8 +59,20 @@ bool serachNode(int nim, Node* current, Node* previous) {
 
     if (current == NULL)
     {
-
+        return false;
     }
+    else if (current->noMhs == nim)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool deleteNode(int nim) {
+
 }
 
 
